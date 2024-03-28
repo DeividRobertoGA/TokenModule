289f1c77e0c33c296b4d68c2baecbd701f116384
Deivid Roberto <deividrobertogalves@gmail.com>
2024-03-28 07:47:41 -0300

    Adicionado a Verificação do JsonWebToken

--- token.config.js
+++ token.config.js
@@ -11,4 +11,24 @@ const secretToken = process.env.TOKEN_SECRET || "REfOrMuGoLAHImP";
 //Criando o TOKEN
 const createJWT = (id) => { const token = jwt.sign({id}, secretToken, {expiresIn: process.env.TOKEN_EXPIRE || 18000}); };
 
-export default createJWT;
+//Verificar o TOKEN
+const verifyJWT = (req, res, next) => {
+    const authToken = req.headers.authorrization;
+
+    if (!authToken) { return res.status(401).json({message: "Token não enviado"}) };
+
+    const [srt, token] = authToken.split(" ");
+
+    jwt.verify(token, secretToken, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({message: "Token não valido"});
+        } else {
+            req.user_id = decoded.id;
+
+            next();
+        }
+    })
+}
+
+
+export {createJWT, verifyJWT};
